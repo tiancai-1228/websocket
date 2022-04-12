7bfb96e325056e01de9a8e6f2acb030f263a99ed
TW-IT-Robby.hong <robby.hong@shoalter.com>
2022-04-12 10:53:58 +0800

    test

--- server.js
+++ server.js
@@ -2,15 +2,16 @@
 
 //import express 和 ws 套件
 const express = require("express");
-const SocketServer = require("ws").Server;
+const { Server } = require("ws");
 const PORT = process.env.PORT || 3000; //指定 port
-
+const INDEX = "/index.html";
 //創建 express 物件，綁定監聽  port , 設定開啟後在 console 中提示
-const server = express().listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 //將 express 交給 SocketServer 開啟 WebSocket 的服務
-const wss = new SocketServer({ server });
+const wss = new Server({ server });
 //當有 client 連線成功時
 wss.on("connection", (ws) => {
   console.log("Client connected");
