3553fa47866fa2418a079a1cea981e5cd16a74c9
TW-IT-Robby.hong <robby.hong@shoalter.com>
2022-04-12 10:59:01 +0800

    web socket

--- server.js
+++ server.js
@@ -25,31 +25,14 @@ wss.on("connection", (ws) => {
     // ws.send(data);
 
     /// 發送給所有client：
-
     let clients = wss.clients; //取得所有連接中的 client
     clients.forEach((client) => {
       client.send(data); // 發送至每個 client
     });
   });
+
   // 當連線關閉
   ws.on("close", () => {
     console.log("Close connected");
   });
 });
-
-// const express = require("express");
-// const { Server } = require("ws");
-
-// const PORT = process.env.PORT || 3000;
-// const INDEX = "/index.html";
-
-// const server = express()
-//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-// const wss = new Server({ server });
-
-// wss.on("connection", (ws) => {
-//   console.log("Client connected");
-//   ws.on("close", () => console.log("Client disconnected"));
-// });
